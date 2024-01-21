d2b3df1194b28da8bdd75c2d36bda63092819df3
Gabieecj98 <157268903+Gabieecj98@users.noreply.github.com>
2024-01-21 02:37:35 -0700

    Add files via upload

---
+++ Module 1 Results.py
@@ -0,0 +1,54 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:39) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================================================== RESTART: /Users/gabrielajohnson/Desktop/1 Module 1.py =======================================================================
+Enter First Number: 10
+Enter Second Number: 8
+Traceback (most recent call last):
+  File "/Users/gabrielajohnson/Desktop/1 Module 1.py", line 9, in <module>
+    print(num1, " + " . num2, " = " , result)
+AttributeError: 'str' object has no attribute 'num2'
+>>> 
+======================================================================== RESTART: /Users/gabrielajohnson/Desktop/1 Module 1.py =======================================================================
+Enter First Number: 10
+Enter Second Number: 8
+10  +  8  =  18
+>>> 
+======================================================================== RESTART: /Users/gabrielajohnson/Desktop/1 Module 1.py =======================================================================
+Enter First Number: 10
+Enter Second Number: 8
+10  +  8  =  18
+Traceback (most recent call last):
+  File "/Users/gabrielajohnson/Desktop/1 Module 1.py", line 10, in <module>
+    print(num1, "-" , num2, " = " , results)
+NameError: name 'results' is not defined. Did you mean: 'result'?
+>>> 
+======================================================================== RESTART: /Users/gabrielajohnson/Desktop/1 Module 1.py =======================================================================
+Enter First Number: 10
+Enter Second Number: 8
+10  +  8  =  18
+10  -  8  =  18
+>>> 
+======================================================================== RESTART: /Users/gabrielajohnson/Desktop/1 Module 1.py =======================================================================
+Enter First Number: 10
+Enter Second Number: 2
+10  +  2  =  12
+10  -  2  =  12
+10  *  2  =  12
+10  /  2  =  12
+>>> 
+======================================================================== RESTART: /Users/gabrielajohnson/Desktop/1 Module 1.py =======================================================================
+Enter First Number: 10
+Enter Second Number: 2
+sum= 12
+subtraction= 8
+multiplication= 20
+division= 5.0
+>>> 
+======================================================================== RESTART: /Users/gabrielajohnson/Desktop/1 Module 1.py =======================================================================
+Enter First Number: 10
+Enter Second Number: 2
+sum= 12
+subtraction= 8
+multiplication= 20
+division= 5.0
