bc2ab2cfcc2e0b9d7a4ef87ad0af6fad15d6047d
Gabieecj98 <157268903+Gabieecj98@users.noreply.github.com>
2024-01-21 02:36:16 -0700

    Add files via upload

---
+++ 1 Module 1.py
@@ -0,0 +1,11 @@
+# Part 1 & 2 : Addition and Subtraction / Multiplication and division
+
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+
+print("sum=" , num1+num2)
+print("subtraction=" , num1-num2)
+print("multiplication=" , num1*num2)
+print("division=" , num1/num2)
